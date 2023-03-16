6a8ebc7160717b1227d18e8d3362997f10e2b08e
allison ruiz <allisonnruizz@gmail.com>
2023-03-16 14:06:17 -0700

    part 4 practice

---
+++ python_projects/python_funtion_practicep4.py
@@ -0,0 +1,23 @@
+def max_num(a,b,c):
+    return max([a,b,c])
+
+print(max_num(1,2,3))
+print(max_num(100,50,1))
+print(max_num(15,30,2))
+
+
+def multi_list(lst):
+    if len(lst) == 0:
+        return 0
+
+    prod = lst[0]
+
+    if len(lst) >1:
+        for i in lst[1:]:
+            prod = prod * i
+
+    return prod
+
+print(multi_list([1,2,3]))
+print(multi_list([]))
+print(multi_list([15]))
